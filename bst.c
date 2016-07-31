c9e5e1b34aeccbe0e79317af454080e5145c9aa7
Rohan Singh <rohan.vishen@gmail.com>
2016-08-01 01:36:22 +0530

    Update bst.c

--- bst.c
+++ bst.c
@@ -12,23 +12,12 @@ typedef struct tnode
 }tnode;
 
 //function prototypes
-int findSize(tnode* s);
 tnode* newNode(int val);
 tnode* insertNode(tnode* root, int val);
 void traverseTree(tnode* root);
 
 //global declarations
 tnode* root = NULL;
-
-//function to find the size of a tree
-int findSize(tnode* s)
-{
-    if (s == NULL)
-        return 0;
-    else
-        return (findSize(s->left) + 1 + findSize(s->right));
-    
-}
 
 //function to create a new node
 tnode* newNode(int val)
@@ -56,7 +45,6 @@ int main()
     while(!strcmp(choice, "yes"));
     printf("\nThe elements in the tree are: ");
     traverseTree(root);
-    printf("\nThe size of the tree is %i", findSize(root));
     return 0;
 }
 
